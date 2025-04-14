b204560c56b062932705a93408ef20505cfbbbb6
Weida Chen <weidac1@uci.edu>
2025-04-13 21:55:44 -0700

    Implemented Dashing for the future use
    PlayerMovement script has a new boolean variable hasDash, if checked, pressing L allows character to dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,23 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 2f;
     public float jump = 5f;
-    private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
     private float moveInput;
     private bool grounded;
-    public Transform groundCheck;
+    [SerializeField] private Transform groundCheck;
     public float checkRadius = 0.2f;
     private bool _facingRight = true;
     public float maxFallSpeed = 20f;
     public LayerMask groundLayer;
+    // For checking if player has the dash ability at all
+    public bool hasDash = false;
+    // For checking if the player can dash when having the dash ability
+    private bool canDash = true;
+    public bool isDashing;
+    [SerializeField] private float dashingPower = 20f;
+    [SerializeField] private float dashingTime = 0.2f;
+    [SerializeField] private float dashingCooldown = .5f;
+    [SerializeField] private TrailRenderer tr;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -25,6 +34,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         moveInput = Input.GetAxisRaw("Horizontal");
 
         if (moveInput > 0 && !_facingRight)
@@ -40,10 +53,21 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
         }
+        if (hasDash)
+        {
+            if (Input.GetKeyDown(KeyCode.L) && canDash)
+            {
+                StartCoroutine(Dash());
+            }
+        }
     }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
     }
@@ -63,4 +87,22 @@ public class PlayerMovement : MonoBehaviour
         Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
     }
 
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+
+    }
 }
