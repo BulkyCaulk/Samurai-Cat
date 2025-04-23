c8e18ada657a2bbdebb6412bbb6f4a172d506833
BulkyCaulk <marcoac6@uci.edu>
2025-04-23 16:31:47 -0700

    Boss almost done
    -need to disable hitbox
    -adjust values for balance
    -also need something for collision(should projectile be destroyed when hitting other objects or just player) maybe ignore boss

---
+++ Assets/Scripts/SpawnProjectile.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private float _projectileSpeed;
+    [SerializeField] private GameObject _spawnPosition;
+    private float _timeSpan;
+    private GameObject _projectile;
+    private Vector2 _projectileDirection;
+    public GameObject ProjectileSpawnPosition {get { return _spawnPosition ;} }
+    public Vector2 ProjectileDirection { set {_projectileDirection = value; } }
+    public GameObject ProjectileGameObject { set {_projectile = value; } }
+    public float TimeSpan { set { _timeSpan = value ; } }
+    public float ProjectileSpeed { set { _projectileSpeed = value ;} }
+
+    void Start()
+    {
+        _timeSpan = 5;
+    }
+    public GameObject SpawnProjectileObject()
+    {
+        _projectile = Instantiate(_projectilePrefab, _spawnPosition.transform.position, Quaternion.identity);
+        return _projectile;
+    }
+
+    void Update()
+    {
+        _timeSpan -= Time.deltaTime;
+        if(_projectile != null)
+        {
+            _projectile.transform.Translate(_projectileDirection * _projectileSpeed * Time.deltaTime);
+
+        }
+        if(_timeSpan <= 0)
+        {
+            _timeSpan = 3;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"Hit {collision.name}");
+        if(collision.name == "Player")
+        {
+            Destroy(gameObject);        
+        }
+        
+    }
+}
