36cdc90c241c372e25cfb7193d4f2fd60b496730
Marco Camacho <marcoac6@uci.edu>
2025-04-28 12:17:05 -0700

    Revert "Reapply "Merge branch 'marco-branch'""
    This reverts commit 5b4fe99eca517ec88610042dbd286a8347f0f718.

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -7,12 +7,11 @@ public class Boss : MonoBehaviour
     [SerializeField] private List<GameObject> _bossPlatformsSpawner;
     [SerializeField] private GameObject _bossPlatformPattern;
     [SerializeField] private float _timerTillNextPlatform;
-    [SerializeField] private float _bossHealth;
+    [SerializeField] private Enemy _bossHealth;
     [SerializeField] private GameObject _fireBallSpawner;
     [SerializeField] private float _platformTimeSpan;
     [SerializeField] private SpawnProjectile _spawnProjectile;
     [SerializeField] private float _fireRate;
-    [SerializeField] private BoxCollider2D _bossCollider;
     private float _timer;
     private IEnumerator _platformCoroutine;
     private IEnumerator _shootAttackCoroutine;
@@ -20,8 +19,7 @@ public class Boss : MonoBehaviour
     private List<GameObject> _platformsSpawned;
     private List<GameObject> _platformsToRemove;
     private Vector3 _playersLastPosition;
-    private Vector3 _pushBackArea;
-
+    
     
     // Start is called before the first frame update
     void Awake()
@@ -30,10 +28,7 @@ public class Boss : MonoBehaviour
         _platformsSpawned = new List<GameObject>();
         _platformsToRemove = new List<GameObject>();
     }
-    void Start()
-    {
-        _pushBackArea = new Vector3(0, 1.5f, 0) + this.transform.position;
-    }
+
     // Update is called once per frame
     void Update()
     {
@@ -42,16 +37,15 @@ public class Boss : MonoBehaviour
 
         if(_timer >= _timerTillNextPlatform) {
             _timer = 0;
-            _bossCollider.enabled = true;
             _platformCoroutine = SpawnPlatforms();
             StartCoroutine(_platformCoroutine);
         }
 
-        Debug.Log(_bossHealth);
+        Debug.Log(_bossHealth.Enemy_Health);
 
         
         // needs to be implemented still
-        if(_bossHealth <= 2 && _fireRate <= 0)
+        if(_bossHealth.Enemy_Health <= 2 && _fireRate <= 0)
         {
             _fireRate = 1.2f;
             _shootAttackCoroutine = ShootAttack();
@@ -122,44 +116,10 @@ public class Boss : MonoBehaviour
             projectileScript.ProjectileGameObject = _projectile;
        }
        // increase the projectile speed 
-       if(_bossHealth == 1)
+       if(_bossHealth.Enemy_Health == 1)
         {
             projectileScript.ProjectileSpeed = 10;
             _fireRate = .4f;
         }
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        _pushBackArea = new Vector3(0, 1.5f, 0) + this.transform.position;
-        Gizmos.DrawWireSphere(_pushBackArea, 3.5f);
-    }
-    void PushPlayerAway()
-    {
-        Collider2D objectInVicinity = Physics2D.OverlapCircle(_pushBackArea, 3.5f);
-        Debug.Log(objectInVicinity.name);
-        if(objectInVicinity.TryGetComponent<Rigidbody2D>(out Rigidbody2D playerRB) && objectInVicinity.name == "Player")
-        {
-            Vector2 direction = Vector3.Normalize(_pushBackArea - objectInVicinity.transform.position);
-            playerRB.velocity = direction * 30;
-            Debug.Log($"{playerRB.name} is being pushed?");
-        }
-    }
-
-    public void TakeDamage()
-    {
-        //reduce enemy health by x amount of damage
-        _bossHealth--;
-        PushPlayerAway();
-        _bossCollider.enabled = false;
-        if(_bossHealth <= 0)
-        {
-            //play death animation
-            //_enemyAnimator.SetBool("isDead", true);
-            //destroy game object
-            Destroy(gameObject);
-        }
-        
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -26,7 +26,6 @@ public class Enemy : MonoBehaviour
             //destroy game object
             Destroy(gameObject);
         }
-        
     }
 
 }
