ed7445b0a7c620ce0390964fad4bd607831ee25d
BulkyCaulk <marcoac6@uci.edu>
2025-05-13 17:09:18 -0700

    In progress
    boss 2 room in progress

---
+++ Assets/Floater.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    private Vector3 _objectStartPosition;
+    private Rigidbody2D _objectRigidbody;
+    
+    void Start()
+    {
+        _objectStartPosition = this.transform.position;
+        _objectRigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //changes gravity scale of object to simulate floating
+        Debug.Log(_objectStartPosition + "      " + this.transform.position);
+        if(_objectStartPosition.y > this.transform.position.y + .04f)
+        {
+            _objectRigidbody.gravityScale = -.01f;
+        }
+        else
+        {
+            _objectRigidbody.gravityScale = .01f;
+        }
+    }
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///          
+/// 
+/// 
+
+
+// Changes the player mass because the platforms would fall
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Rigidbody2D playerRB = collision.GetComponent<Rigidbody2D>();
+            Debug.Log("enter");
+            playerRB.mass = .002f;
+        }
+    }
+
+
+// Reverts player mass when player leaves platform
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Rigidbody2D playerRB = collision.GetComponent<Rigidbody2D>();
+            Debug.Log("exit");
+            playerRB.mass = 1;
+        }
+    }
+}
