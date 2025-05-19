1ad84cf7841e644d7104d83282293c3185a56aee
Marco Camacho <marcoac6@uci.edu>
2025-05-19 12:11:04 -0700

    Set attack positions and created boss 2 script
    - changed player floor box in boss scene 2 to be on
    - created some boss aspects

---
+++ Assets/Scripts/Boss 2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2 : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _bossAttackPositions;
+    [SerializeField] private List<GameObject> _bossSlerpPositions;
+    [SerializeField] private GameObject _bossSpitPosition;
+    
+}
